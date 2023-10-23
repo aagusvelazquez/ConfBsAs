5ab13fb6b95254abcf137a7d32e81245368b16e5
aagusvelazquez <velazquezagustinamailen@gmail.com>
2023-10-23 18:25:04 -0300

    Agregué funciones de JavaScript que validan que los campos del formulario sean compleatos con datos correctos. Funcionan todas las funciones al presionar el boton Resumen. Agregué botón 'Comprar' para crear un ticket que muestre los datos de la compra, pero aún no creé el HTML correspondiente.

--- js/main.js
+++ js/main.js
@@ -1,78 +1,88 @@
 function resumen() {
     var nombre = document.getElementById('nombre');
+    var nombreError = document.getElementById('nombreError');
     var apellido = document.getElementById('apellido');
+    var apellidoError = document.getElementById('apellidoError');
     var correo = document.getElementById('email');
-    var nombreVacio = document.getElementById('nombreVacio');
-    var apellidoVacio = document.getElementById('apellidoVacio');
-    var correoVacio = document.getElementById('correoVacio');
+    var correoError = document.getElementById('correoError');
 
-    if (!nombre.value) {
-        nombreVacio.classList.add('visible');
-        nombre.classList.add('invalido');
-        nombreVacio.setAttribute('aria-hidden', false);
-        nombreVacio.setAttribute('aria-invalid', true);
-    }else{
-        nombreVacio.classList.add('invisible');
-        nombre.classList.remove("invalido");
-        nombreVacio.setAttribute('aria-hidden', true);
-        nombreVacio.setAttribute('aria-invalid', true);
+    var expRegNombre=/^[a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+    var expRegApellido=/^[a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+    var expRegCorreo=/^\w+@(\w+\.)+\w{2,4}$/;
+
+    // Campo Nombre
+    if (!nombre.value || !expRegNombre.exec(nombre.value)) {
+        nombreError.classList.add('visible');
+        nombre.classList.add('is-invalid');
+        nombreError.setAttribute('aria-hidden', false);
+        nombreError.setAttribute('aria-invalid', true);
+    }else {
+        nombreError.classList.add('invisible');
+        nombre.classList.remove('is-invalid');
+        nombreError.setAttribute('aria-hidden', true);
+        nombreError.setAttribute('aria-invalid', true);
     }
 
-    if (!apellido.value) {
-        apellidoVacio.classList.add('visible');
-        apellido.classList.add('invalido');
-        apellidoVacio.setAttribute('aria-hidden', false);
-        apellidoVacio.setAttribute('aria-invalid', true);
-    }else{
-        apellidoVacio.classList.add('invisible');
-        apellido.classList.remove("invalido");
-        apellidoVacio.setAttribute('aria-hidden', true);
-        apellidoVacio.setAttribute('aria-invalid', true);
+    // Campo Apellido
+    if (!apellido.value || !expRegApellido.exec(apellido.value)) {
+        apellidoError.classList.add('visible');
+        apellido.classList.add('is-invalid');
+        apellidoError.setAttribute('aria-hidden', false);
+        apellidoError.setAttribute('aria-invalid', true);
+    }else {
+        apellidoError.classList.add('invisible');
+        apellido.classList.remove('is-invalid');
+        apellidoError.setAttribute('aria-hidden', true);
+        apellidoError.setAttribute('aria-invalid', true);
     }
 
-    if (!correo.value) {
-        correoVacio.classList.add('visible');
-        correo.classList.add('invalido');
-        correoVacio.setAttribute('aria-hidden', false);
-        correoVacio.setAttribute('aria-invalid', true);
-    }else{
-        correoVacio.classList.add('invisible');
-        correo.classList.remove("invalido");
-        correoVacio.setAttribute('aria-hidden', true);
-        correoVacio.setAttribute('aria-invalid', true);
+    // Campo Correo
+    if (!correo.value || !expRegCorreo.exec(correo.value)) {
+        correoError.classList.add('visible');
+        correo.classList.add('is-invalid');
+        correoError.setAttribute('aria-hidden', false);
+        correoError.setAttribute('aria-invalid', true);
+    }else {
+        correoError.classList.add('invisible');
+        correo.classList.remove('is-invalid');
+        correoError.setAttribute('aria-hidden', true);
+        correoError.setAttribute('aria-invalid', true);
     }
 
     var cant = document.getElementById('cantidad');
     var cantVacio = document.getElementById('cantVacio');
     var cat = document.getElementById('categoria');
+    var resumenTotal;
 
-    if (cat.value == 0) {
-        resumenTotal = (200*0.2)*cant.value;
-        document.getElementById('totalPagar').innerHTML = `$${resumenTotal}`;
-    } else if (cat.value == 1) {
-        resumenTotal = (200*0.5)*cant.value;
-        document.getElementById('totalPagar').innerHTML = `$${resumenTotal}`;
-    } else if (cat.value == 2) {
-        resumenTotal = (200*0.85)*cant.value;
-        document.getElementById('totalPagar').innerHTML = `$${resumenTotal}`;
-    }else if (cat.value == 3) {
-        resumenTotal = 200*cant.value;
-        document.getElementById('totalPagar').innerHTML = `$${resumenTotal}`;
-    }
-
+    // Campo Cantidad y seleccion de Categoria
     if (!cant.value) {
         cantVacio.classList.add('visible');
-        cant.classList.add('invalido');
+        cant.classList.add('is-invalid');
         cantVacio.setAttribute('aria-hidden', false);
         cantVacio.setAttribute('aria-invalid', true);
+        resumenTotal = "Seleccione la cantidad de entradas";
+        document.getElementById('totalPagar').innerHTML = `${resumenTotal}`;
     }else{
         cantVacio.classList.add('invisible');
-        cant.classList.remove("invalido");
+        cant.classList.remove("is-invalid");
         cantVacio.setAttribute('aria-hidden', true);
         cantVacio.setAttribute('aria-invalid', true);
+
+        if (cat.value == 0) {
+            resumenTotal = (200*0.2)*cant.value;
+            document.getElementById('totalPagar').innerHTML = `$${resumenTotal}`;
+        } else if (cat.value == 1) {
+            resumenTotal = (200*0.5)*cant.value;
+            document.getElementById('totalPagar').innerHTML = `$${resumenTotal}`;
+        } else if (cat.value == 2) {
+            resumenTotal = (200*0.85)*cant.value;
+            document.getElementById('totalPagar').innerHTML = `$${resumenTotal}`;
+        }else if (cat.value == 3) {
+            resumenTotal = 200*cant.value;
+            document.getElementById('totalPagar').innerHTML = `$${resumenTotal}`;
+        }
     }
 }
 
 const btnClick = document.getElementById('btnResumen');
 btnClick.addEventListener('click', resumen);
-
